78276fd7e6e477742800eebfe4da176fc6d1ff24
Moussassoss <ssossinfo88@gmail.com>
2025-10-18 12:38:54 +0200

    updated schema

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,7 @@
 import graphene
+from crm.schema import CRMQuery  # make sure this import matches your crm/schema.py location
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
+class Query(CRMQuery, graphene.ObjectType):
+    pass
 
 schema = graphene.Schema(query=Query)
